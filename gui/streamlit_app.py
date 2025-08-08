d6996bd817c00e702de15a86fb7dc64dcbfadfd6
prachi07042004 <prachi.07sharma.07@gmail.com>
2025-08-08 21:04:45 +0530

    changed path

--- gui/streamlit_app.py
+++ gui/streamlit_app.py
@@ -4,7 +4,9 @@ import sys
 import os
 import time
 import pandas as pd
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+# sys.path.append(os.path.dirname(__file__), ".."))
+root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(root_dir)
 from backend.mongodb_connect import db
 from dotenv import load_dotenv
 from backend.ws_recorder import start_recording, stop_recording, shared_conn 
